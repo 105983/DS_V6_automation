1b2c1063a19231ce942e5cae6b7d5e4dc1b2c9bf
105983 <105983@gmail.com>
2017-07-24 17:53:19 +0530

    Add files via upload

---
+++ features/build_trunk.feature
@@ -0,0 +1,5 @@
+Feature: Bamboo build_trunk automation
+
+  Scenario: Build trunk automation
+    Given I am on the bamboo page
+    Then I run the plan for trunk build
---
+++ features/support/env.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'capybara/cucumber'
+require 'rspec/expectations'
+require 'capybara/rspec'
+require 'selenium/webdriver'
+require 'yaml'
+require 'rspec'
+# require 'mail'
+include RSpec::Matchers
+=begin
+require 'json'
+require 'roo'
+require "yaml/store"
+require 'debase_internals'
+require 'win32ole'
+=end
+
+#<----------------------End of Function---------------------->
+
+Capybara.default_driver = :selenium
+# Capybara.javascript_driver = :webkit
+Capybara.javascript_driver = :selenium
+Capybara.default_max_wait_time = 20
+# Capybara.default_selector = :css
+ Capybara.app_host='http://ds-prod-bamboo-1/'
+#<----------------------End of Function---------------------->
+
+#<----------------------End of Function---------------------->
+
+# Browser initialization
+Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(app,:browser=>:internet_explorer)
+end
+
+Selenium::WebDriver::Driver.class_eval do
+  def quit
+    STDOUT.puts "#{self.class}#Browser will not be Quited!!!"
+  end
+end
+
+Capybara::Selenium::Driver.class_eval do
+    def reset!
+  end
+end
