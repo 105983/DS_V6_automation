f6eb4b7b3ff04d3b419adae2ad0ea758ff670311
105983 <105983@gmail.com>
2017-07-26 12:06:11 +0530

    Update Test_bamboo.rb

--- features/step_definitions/Test_bamboo.rb
+++ features/step_definitions/Test_bamboo.rb
@@ -2,7 +2,6 @@ When(/^I am on the google page$/) do
   visit 'search?site=&source=hp&q=test&oq=test&gs_l=psy-ab.3..35i39k1l6.1342.1594.0.1716.6.5.0.0.0.0.311.311.3-1.1.0....0...1.1.64.psy-ab..5.1.311.6.AVimYRFH1go'
 end
 
-
 When (/^Search for text test$/) do
   expect(page).to have_content ('test')
 end
